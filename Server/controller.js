ba4b907e97af27a2e39852f70f0533e5c3cc2a17
Chris McKnight <tenlongfingers10@gmail.com>
2022-07-04 19:32:34 -0600

    Added server boiler code

---
+++ Server/controller.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(tarot - images.json());
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
